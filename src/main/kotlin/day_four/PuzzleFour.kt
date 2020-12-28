335df326e246b45dd3754c0554df9d26f3ecbee5
sayedtenkanen <ext_sayed.tenkanen@suunto.com>
2020-12-28 02:54:59 +0200

    Puzzle four class

---
+++ src/main/kotlin/day_four/PuzzleFour.kt
@@ -0,0 +1,14 @@
+package day_four
+
+import utils.runIfFileAvailable
+
+class PuzzleFour {
+
+    fun solvePuzzleFour() {
+        val filePathNameString = "src/main/kotlin/day_three/input_four.txt"
+
+        runIfFileAvailable(puzzleFourActions().toString()) { }
+    }
+
+    private fun puzzleFourActions() { }
+}
