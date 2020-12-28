0f8e6ad37d8f3dd560ae9908edb679cc858df02b
S Tenkanen <43746111+sayedtenkanen@users.noreply.github.com>
2020-12-28 04:34:31 +0200

    Update PuzzleFour.kt

--- src/main/kotlin/day_four/PuzzleFour.kt
+++ src/main/kotlin/day_four/PuzzleFour.kt
@@ -7,7 +7,7 @@ class PuzzleFour {
     fun solvePuzzleFour() {
         val filePathNameString = "src/main/kotlin/day_three/input_four.txt"
 
-        println("print stuff")
+        println("print stuffa")
 
         runIfFileAvailable(puzzleFourActions().toString()) { }
     }
